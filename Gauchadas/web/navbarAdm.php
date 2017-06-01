0aa8ef2dcf6741e0ad5518a6ddb5eff850b2bb58
Fran <ftagliero@gmail.com>
2017-06-01 17:54:06 -0300

    navAdm

---
+++ Gauchadas/web/navbarAdm.php
@@ -0,0 +1,24 @@
+<nav class="navbar fixed-top navbar-toggleable-md navbar-light" id="mainNav" 
+    style="background-image: linear-gradient(180deg,rgba(0,0,0,.4) 0,transparent); border: none;">
+        <div class="container">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="index.php">Gauchadas</a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="index.php">
+                        <i class="fa fa-home" aria-hidden="true"></i>Listar Usuario </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="">
+                        <i class="fa fa-user" aria-hidden="true"></i> Ranking de mejores usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="logout.php">
+                        <i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
